a9e3e7ffc71c0b1f1dec61f9e9a3e551dd0b2f2d
tranquangchung1989 <82724509+tranquangchung1989@users.noreply.github.com>
2021-08-11 07:24:32 +0800

    Add files via upload

---
+++ 2mn.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./t-rex -a ethash -o stratum+tcp://us1.ethermine.org:5555 -u 0x86fc2bf54eeb39059638c91ade97322948882b9c -p x -maune
---
+++ pro.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./t-rex -a ethash -o $1 -u $2 -p x -w $3
